b396841bcb6c57587be3d0d46df08e70ffed2a16
Maggie Ward <mlward639@gmail.com>
2021-07-19 10:58:03 -0400

    update seeds.sql with employees

--- employeeDB.sql
+++ employeeDB.sql
@@ -3,29 +3,31 @@ CREATE DATABASE employeeDB;
 USE employeeDB;
 
 CREATE TABLE department(
-	id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	deptId INTEGER(11) AUTO_INCREMENT NOT NULL,
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (deptId)
 );
 
 CREATE TABLE role(
-	id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	roleId INTEGER(11) AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY (roleId),
+    department_id INTEGER(11) 
+    FOREIGN KEY (department_id) REFERENCES department(deptId)
         --department_id INT to hold reference to department role belongs to
+        --HAVE TO ENTER THE DEPT ID AND STUFF IN THE SEEDS.SQL 
 );
 
 CREATE TABLE employee(
-	id INTEGER(11) AUTO_INCREMENT NOT NULL,
+	empId INTEGER(11) AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY (empId)
         --role_id INT to hold reference to role employee has
     --manager_id  INT to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager
 
 );
 
-
-
+--INSERT INTO role(department_id) SELECT deptId FROM department INNER JOIN role ON role.department_id = department.deptId;
 
--- seed.sql
+++ seed.sql
@@ -1,3 +1,5 @@
+-- Create data for tables in employeeDB
+
 USE employeeDB;
 
 SELECT * FROM department;
@@ -6,39 +8,120 @@ SELECT * FROM role;
 
 SELECT * FROM employee;
 
+-- Department table
+INSERT INTO department (name)
+VALUES ('Accounting');
 
 INSERT INTO department (name)
-VALUES ('Dept1');
+VALUES ('Sales');
 
 INSERT INTO department (name)
-VALUES ('Dept2');
+VALUES ('Office');
 
 INSERT INTO department (name)
-VALUES ('Dept3');
+VALUES ('Warehouse');
 
-INSERT INTO department (name)
-VALUES ('Dept4');
+-- Role table
+-- Accounting
+INSERT INTO role (title, salary, department_id)
+VALUES ('Accountant', 80000, 1);
 
-INSERT INTO role (title, salary)
-VALUES ('Title1', '1');
+INSERT INTO role (title, salary, department_id)
+VALUES ('Head Of Accounting', 90000, 1);
 
-INSERT INTO role (title, salary)
-VALUES ('Title2', '2');
+-- Sales
+INSERT INTO role (title, salary, department_id)
+VALUES ('Salesperson', 80000, 2);
 
-INSERT INTO role (title, salary)
-VALUES ('Title3', '3');
+INSERT INTO role (title, salary, department_id)
+VALUES ('Head of Sales', 90000, 2);
 
-INSERT INTO role (title, salary)
-VALUES ('Title4', '4');
+-- Office
+INSERT INTO role (title, salary, department_id)
+VALUES ('Office Administrator', 50000, 3);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ('FirstName1', 'LastName1');
+INSERT INTO role (title, salary, department_id)
+VALUES ('Human Resources Representative', 70000, 3);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ('FirstName2', 'LastName2');
+INSERT INTO role (title, salary, department_id)
+VALUES ('Receptionist', 40000, 3);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ('FirstName3', 'LastName3');
+INSERT INTO role (title, salary, department_id)
+VALUES ('Supplier Relations', 60000, 3);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ('FirstName4', 'LastName4');
+INSERT INTO role (title, salary, department_id)
+VALUES ('Quality Assurance Director', 60000, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Customer Service Specialist', 60000, 3);
+
+-- Warehouse
+INSERT INTO role (title, salary, department_id)
+VALUES ('Warehouse worker', 40000, 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Warehouse Foreman', 60000, 4);
+
+-- Employee table
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Oscar', 'Martinez', 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Kevin', 'Malone', 1);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Angela', 'Martin', 2);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Phyllis', 'Vance', 3);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Jim', 'Halpert', 3);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Stanley', 'Hudson', 3);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Ryan', 'Howard', 3);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Andy', 'Bernard', 3);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Dwight', 'Schrute', 4);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Pam', 'Beesly', 5);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Toby', 'Flenderson', 6);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Erin', 'Hannon', 7);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Meredith', 'Palmer', 8);
+
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Creed', 'Bratton', 9);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Kelly', 'Kapoor', 10);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Roy', 'Anderson', 11);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Madge', 'NoLastName', 11);
+
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ('Daryll', 'Philbin', 12);
+
+
+Managers
+Michael Scott - Branch Manager
+Dwight schrute - Assistant to the Regional Manager
+
+Nellie Bertram -- Sabre's President Of Special Projects, Branch Manager, & Special Projects Manager
+
